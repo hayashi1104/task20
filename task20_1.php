ea7bb3d9248e9ad9881dace878e97952e5566aa2
hayashi1104 <ybnww962@gmail.com>
2019-07-26 22:59:50 +0900

    first

---
+++ task20_1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>task20_1</title>
+</head>
+
+<body>
+
+<h1>初めてのGitHub</h1>
+
+<?php
+echo 'GitHub学習1';
+?>
+
+
+</body>
+</html>
