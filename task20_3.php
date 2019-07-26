33d4d7776a656171d9e9c2446a18f21965e60df4
hayashi1104 <ybnww962@gmail.com>
2019-07-26 23:12:41 +0900

    はじめて

---
+++ task20_3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>task20_3</title>
+</head>
+
+<body>
+
+<h1>初めてのGitHub</h1>
+
+<?php
+echo 'GitHub学習3';
+?>
+
+
+</body>
+</html>
